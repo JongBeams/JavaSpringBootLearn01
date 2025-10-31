aed026afa8c2c8e77ad551e3068f0440124d583f
JongBeams <wwhow2003@gmail.com>
2025-10-31 17:14:48 +0900

    Spring Boot rest API basic

---
+++ src/main/java/io/jongbeom/springboot/intellij/leranspringboot/CourseController.java
@@ -0,0 +1,43 @@
+package io.jongbeom.springboot.intellij.leranspringboot;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+// http://localhost:8080/courses
+// URL Course 를 사용하여 간단한 Rest API 제작
+// URL 입력 시 id, name, author 반환
+/*
+    [
+         {
+         "id": 1,
+         "name": "Learn AWS",
+         "author": "in28minutes"
+         }
+    ]
+*/
+
+@RestController
+public class CourseController {
+
+
+    // Course 반환 목록 : id, name, author
+    // URL /courses 입력 시 반환
+    @RequestMapping("/courses")
+    public List<Course> retrieveAllCourses(){
+        return Arrays.asList(
+                new Course(1,"Learn AWS","JangJongBeom"),
+                new Course(2,"Learn DevOps","JangJongBeom")
+
+        );
+    }
+
+
+
+
+
+}
--- src/main/java/io/jongbeom/springboot/intellij/leranspringboot/LeranSpringBootApplication.java
+++ src/main/java/io/jongbeom/springboot/intellij/leranspringboot/LeranSpringBootApplication.java
@@ -7,7 +7,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeranSpringBootApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(LeranSpringBootApplication.class, args);
+
+
+
+
+
     }
 
 }
