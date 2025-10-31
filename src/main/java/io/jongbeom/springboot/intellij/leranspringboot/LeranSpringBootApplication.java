29fbbe54688d93b8b9dbfac07e96459aebd8b50f
JongBeams <wwhow2003@gmail.com>
2025-10-31 16:44:48 +0900

    Spring Boot Learn Start

---
+++ src/main/java/io/jongbeom/springboot/intellij/leranspringboot/LeranSpringBootApplication.java
@@ -0,0 +1,13 @@
+package io.jongbeom.springboot.intellij.leranspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeranSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeranSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/jongbeom/springboot/intellij/leranspringboot/LeranSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jongbeom.springboot.intellij.leranspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeranSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
